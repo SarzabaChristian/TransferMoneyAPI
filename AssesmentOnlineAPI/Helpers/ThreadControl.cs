bb708bc9cbca327e914d2adfd4fb382a2b6ad87c
Christian Sarzaba <cdsarzaba@shakeys.biz>
2020-09-14 15:14:55 +0800

    push threadcontrol

---
+++ AssesmentOnlineAPI/Helpers/ThreadControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AssesmentOnlineAPI.Helpers
+{
+    public class ThreadControl
+    {   
+        public static long threadCount = 0;
+        public static object obj = threadCount;         
+        public static List<Task> threads = new List<Task>();       
+    }
+}
